96c565083d59a61382ad577fc3dc3348295a542c
krasniukk <tania.krasniuk@gmail.com>
2021-02-24 00:09:00 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='Data from json show',
+   version='1.0',
+   description='Returns all the values found from given key',
+   author='Krasniukk',
+   author_email='tetiana.krasniuk@ucu.edu.ua',
+   packages=['Skyscrapers'],  #same as name
+   install_requires=''
+)
